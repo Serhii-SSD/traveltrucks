0226bc5e262dc220a7e6062068322c19917cea5d
Serhii-SSD <demydenko.ss@gmail.com>
2026-09-06 00:23:51 +0300

    added module of camper reviews

--- components/CamperDetails/CamperReviews/CamperReviews.tsx
+++ components/CamperDetails/CamperReviews/CamperReviews.tsx
@@ -6,55 +6,43 @@ interface CamperReviewsProps {
 }
 
 export default function CamperReviews({ reviews }: CamperReviewsProps) {
-  const renderStars = (rating: number) => {
-    return Array.from({ length: 5 }, (_, index) => {
-      const isFilled = index < rating;
-      return (
-        <span
-          key={index}
-          className={isFilled ? css.starFilled : css.starEmpty}
-        >
-          ★
-        </span>
-      );
-    });
-  };
-
   if (!reviews || reviews.length === 0) {
-    return (
-      <div className={css.noReviews}>
-        <p>No reviews available for this camper yet.</p>
-      </div>
-    );
+    return <p className={css.noReviews}>No reviews yet.</p>;
   }
 
   return (
-    <div className={css.reviewsContainer}>
-      <ul className={css.reviewsList}>
-        {reviews.map((item) => {
-          const initialLetter = item.reviewer_name
-            ? item.reviewer_name.charAt(0).toUpperCase()
-            : 'U';
+    <div className={css.reviewsList}>
+      {reviews.map((review, index) => {
+        const initial = review.reviewer_name
+          ? review.reviewer_name.charAt(0).toUpperCase()
+          : 'U';
 
-          return (
-            <li key={item.id} className={css.reviewCard}>
-              <div className={css.headerRow}>
-                {}
-                <div className={css.avatar}>{initialLetter}</div>
-
-                <div className={css.meta}>
-                  <p className={css.authorName}>{item.reviewer_name}</p>
-                  <div className={css.starsContainer}>
-                    {renderStars(item.reviewer_rating)}
-                  </div>
+        return (
+          <div key={index} className={css.reviewCard}>
+            <div className={css.reviewerHeader}>
+              <div className={css.avatar}>{initial}</div>
+              <div className={css.reviewerInfo}>
+                <p className={css.reviewerName}>{review.reviewer_name}</p>
+                <div className={css.starsRow}>
+                  {Array.from({ length: 5 }, (_, i) => (
+                    <span
+                      key={i}
+                      className={
+                        i < review.reviewer_rating
+                          ? css.starFilled
+                          : css.starEmpty
+                      }
+                    >
+                      ★
+                    </span>
+                  ))}
                 </div>
               </div>
-
-              <p className={css.comment}>{item.comment}</p>
-            </li>
-          );
-        })}
-      </ul>
+            </div>
+            <p className={css.comment}>{review.comment}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
